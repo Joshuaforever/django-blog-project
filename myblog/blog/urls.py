d6855d793fa20b7249c73230f03c033d4f80f4c8
AITI Student <aiti-student@AITI-71.(none)>
2012-07-13 17:11:51 +0000

    josh

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
     url(r'^$', 'blog.views.home'),
     url(r'^posts/$', 'blog.views.post_list'),
     url(r'^posts/(?P<id>\d+)/((?P<showComments>.*)/)?$', 'blog.views.post_detail'),
-    url(r'^posts/search/(?P<token>\d+).','blog.views.post_search'),
+    #url(r'^posts/search/(?P<token>\w+).','blog.views.post_search'),
     
     ## add your url here
     url(r'^posts/search/(\w+)', 'blog.views.post_search')
--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,29 +1,52 @@
 # Create your views here.
 from django.template import Context, loader
 from django.http import HttpResponse
-
 from models import Post, Comment
-
+from django.template import Context, loader, Template
+from django.shortcuts import render_to_response
+from django.forms import ModelForm
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponseRedirect
 
 def post_list(request):
+    posts = Post.objects.all()
+    t = loader.get_template('blog/post_list.html')
+    c = Context({'posts':posts })
+    return HttpResponse(t.render(c))
+
+'''def post_list(request):
     post_list = Post.objects.all()
     print type(post_list)
     print post_list
     lis=[]
     for e in post_list:
         lis.append(e.title)
-    return HttpResponse(lis)
+    return HttpResponse(lis)'''
+
+class CommentForm(ModelForm):
+  class meta:
+    model=Comment
+
+@csrf_exempt
 
 def post_detail(request, id, showComments=False):
+    if request.method == 'POST':	
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return HttpResponseRedirect(request.path)
+    else:
+        form = CommentForm()
+
     post=Post.objects.get(pk=id)
     if(showComments):
      out='<h1>'+post.title+'</h1>'+'<br>'+post.body[:100]+'<br>'+post.body[100:200]
     else:
         out=post.title+'\n'
     return HttpResponse(out)
+
 def post_search(request, term):
     pass
 
 def home(request):
-    print 'it works'
-    return HttpResponse('hello world. Ete zene?') 
+    return render_to_response('blog/base.html',{}) 
